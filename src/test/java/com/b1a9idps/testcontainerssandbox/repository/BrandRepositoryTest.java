0451f9975104632f1f9e3f42c9b416f2b9be6667
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2019-02-03 02:31:22 +0900

    add test

--- src/test/java/com/b1a9idps/testcontainerssandbox/repository/BrandRepositoryTest.java
+++ src/test/java/com/b1a9idps/testcontainerssandbox/repository/BrandRepositoryTest.java
@@ -1,21 +1,29 @@
 package com.b1a9idps.testcontainerssandbox.repository;
 
+import com.b1a9idps.testcontainerssandbox.domain.Brand;
+import org.assertj.core.api.Assertions;
+import org.assertj.core.groups.Tuple;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.MySQLContainer;
-import org.testcontainers.junit.jupiter.Container;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Testcontainers
+@DataJpaTest(excludeAutoConfiguration = AutoConfigureTestDatabase.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class BrandRepositoryTest {
 
-	@Container
-	private static final MySQLContainer MY_SQL_CONTAINER = new MySQLContainer();
+	@Autowired
+	private BrandRepository brandRepository;
 
 	@Test
+	@Sql(scripts = "classpath:/db/migration/brand/initial_data.sql")
 	void test() {
-		assertTrue(MY_SQL_CONTAINER.isRunning());
+		Assertions.assertThat(brandRepository.findAll())
+				.extracting(Brand::getName, Brand::getDesigner, Brand::getGender)
+				.containsExactly(Tuple.tuple("ETHOSENS", "橋本 唯", Brand.Gender.MAN));
 	}
 
 }
---
+++ src/test/resources/db/migration/brand/initial_data.sql
@@ -0,0 +1,9 @@
+create table brand (
+  id integer not null auto_increment,
+  name varchar(30) not null,
+  designer varchar(30) not null,
+  gender varchar(10) not null,
+  primary key (id)
+);
+
+insert into brand (name, designer, gender) values ('ETHOSENS', '橋本 唯', 'MAN')
